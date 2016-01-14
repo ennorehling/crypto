3aaa4ca1b8622c959540688d02d8c5dce14682a5
Enno Rehling <enno.rehling@gmail.com>
2016-01-14 15:55:20 +0100

    drepper code works fine on gcc, no need to have workarounds

--- drepper.h
+++ drepper.h
@@ -1,5 +1,11 @@
 #include <stddef.h>
 
+#ifndef _MSC_VER
+/* probably a gcc derivation */
+#define HAVE_MEMPCPY
+#define HAVE_STPNCPY
+#endif
+
 #ifndef HAVE_MEMPCPY
 void *mempcpy(void *dest, const void *src, size_t n);
 #endif
